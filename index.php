ec374677fd81d88c9747c155604e3f4886504153
Phạm Gia Huy <88099196+hml69@users.noreply.github.com>
2025-04-30 19:40:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>2FA | Lấy mã bảo mật 2 lớp</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap 4 CSS -->
+    <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <!-- SweetAlert2 CSS + JS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
